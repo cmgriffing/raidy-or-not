4b9c73f9fcc47f16834390039e951c2b0941a7e0
Chris Griffing <cmgriffing@gmail.com>
2022-04-20 12:40:20 -0700

    fix: remove Menu import from component since we do it at plugin time

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -217,7 +217,6 @@ import { ApiPath } from "@/types/api";
 import { Raid } from "@/server/repositories/raids";
 import { User } from "@/server/repositories/users";
 import jwtDecode from "jwt-decode";
-import Menu from "primevue/menu";
 import { useToast } from "primevue/usetoast";
 import WindowsIcon from "@/assets/windows.svg";
 import MacosIcon from "@/assets/macos.svg";
